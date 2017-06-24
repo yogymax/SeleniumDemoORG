be9d015b7f84f0ad18f45b4c8b8fd7e1d55576bb
Demo <demo>
2017-06-24 12:46:57 +0530

    Commiting index change in constants

--- src/main/java/com/scp/selenium/OrangeHRM/AppConstants.java
+++ src/main/java/com/scp/selenium/OrangeHRM/AppConstants.java
@@ -6,7 +6,10 @@ public class AppConstants {
 	public static final String USERNAME_CANNOT_BE_BLANK ="Username cannot be empty";
 	public static final String PASSWORD_CANNOT_BE_BLANK ="Password cannot be empty";
 	public static final String APPLICATION_URL = "http://opensource.demo.orangehrmlive.com/index.php/";
-
+	public static final String INDEX = "http://opensource.demo.orangehrmlive.com/index.php/";
+	
+	
+	
 	public enum MyBrowsers{
 		FIREFOX,
 		CHROME,
